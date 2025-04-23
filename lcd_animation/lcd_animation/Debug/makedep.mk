b69825b37e4c3aaec5c3aa50054c9bba093e803a
MostafaSamirKamel <mostafasameer858@gmail.com>
2025-04-23 23:27:12 +0200

    add LCD Animation project

---
+++ lcd_animation/lcd_animation/Debug/makedep.mk
@@ -0,0 +1,10 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+APP\main.c
+
+HAL\LCD\LCD_Program.c
+
+MCAL\DIO\DIO_Program.c
+
